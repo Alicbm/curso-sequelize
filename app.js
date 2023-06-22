270d0b841145fbb4c89c8e9eceec456264fd4e53
alicbm <abarandica1234@gmail.com>
2023-06-22 15:33:31 -0500

    Sequelize ORM #1 Integracion con Expressjs

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express();
 const sequelize = require('./database/db')
+const User = require('./database/models/User')
 
 //Puerto
 const port = process.env.PORT || 3000
 
 //Rutas
 app.get('/', (req, res) => {
-  res.send('El pepe sabroson')
+  //CREAMOS UN USUARIO
+  
+  // User.create({
+  //   username: 'David',
+  //   birthday: new Date(2003, 12, 10)
+  // }).then(user => {
+  //   res.json(user)
+  // })
+
+  // vemos a los usuarios
+  User.findAll().then(user => res.json(user))
+
 })
 
 //Corremos el servidor
@@ -15,7 +27,8 @@ app.listen(port, () => {
   console.log('Runing in ' + port);
 
   // Conectamos a la base de datos
-  sequelize.authenticate().then(() => {
+  // force true: DROP TABLE, borra las tablas
+  sequelize.sync({ force: false }).then(() => {
     console.log('Nos hemos conectado a la DB');
   }).catch(err => {
     console.log('Se ha producido un error: ' + err);
---
+++ database/models/User.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class User extends Model{}
+User.init({
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  birthday: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, { sequelize, modelName: 'user' })
+
+module.exports = User
+
